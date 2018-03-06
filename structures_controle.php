65583e40c4658a84961e50c58cf32d88706901b7
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2018-03-06 11:02:42 +0100

    conditions

--- structures_controle.php
+++ structures_controle.php
@@ -48,3 +48,47 @@ var_dump($a !== $c || $a === $b); // false
 // true || true => true
 // true || false => true
 // false || false => false
+
+if ($a === $c) {
+    echo 'les valeurs sont égales';
+} else {
+    echo 'les valeurs ne sont pas égales';
+}
+
+if ($a > $c) {
+    echo 'supérieur';
+} elseif ($a < $c) {
+    echo 'inférieur';
+} else {
+    echo 'égal';
+}
+
+$name = 'sylvain';
+$chaineVide = '';
+
+if ($name) {
+    echo 'tu as un nom';
+}
+
+if (!$chaineVide) {
+    echo 'tu n\'as pas de valeur';
+}
+
+$totzlek = 'ekf';
+
+var_dump(isset($totzlek));
+var_dump(empty($totzlek));
+
+if ($a == 10) {
+    echo 'ok';
+}
+
+// yoda condition
+# commentaire
+
+if (10 == $a) {
+    echo 'ok';
+} else {
+    echo 'ko';
+}
+
